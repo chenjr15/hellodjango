bfd394a77733c6dbd1f8d038b0cb9da8fb66932b
chenjr15 <chenjr15@live.cn>
2019-05-03 21:33:58 +0800

    在admin中加入层次提示信息

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from .models import Question,Choice
+from .models import Question, Choice
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None                               , {'fields': ['question_text']}),
+        ('Date that publish(past or future)', {'fields': ['pub_date']}),
+    ]
+
 
 admin.site.register(Question, QuestionAdmin)
