83f954b06778772e0e380da022898f670d5cbc46
chenjr15 <chenjr15@live.cn>
2019-05-04 20:09:12 +0800

    加上了关联的选项

--- polls/admin.py
+++ polls/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
+    # 'classes':['collapse'] 指明这个选项可以折叠起来
     fieldsets = [
         (None                               , {'fields': ['question_text']}),
-        ('Date that publish(past or future)', {'fields': ['pub_date']}),
+        ('Date that publish(past or future)', {'fields': ['pub_date'],'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
